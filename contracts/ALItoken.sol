e31e5b18a9ffdfb6f3244fbb3ffdea89b5febaed
luandt <theluan143@gmail.com>
2021-06-29 11:19:00 +0700

    implement mint function

--- contracts/ALItoken.sol
+++ contracts/ALItoken.sol
@@ -7,8 +7,39 @@ contract AliToken is BEP20('Alita Token', 'ALI') {
     uint public period;
     uint releaseDate;
 
-    uint [] releasedTokenperPeriod = [1000000000, 800000000,640000000,512000000,
-    409600000,327680000,262144000,209715200,167772160,134217728];
+    uint [] releasedTokenperPeriod = [
+
+
+    100*10*18, 
+    80*10*18,
+    640000000,
+    512000000,
+    409600000,
+    327680000,
+    262144000,
+    209715200,
+    167772160,
+    134217728
+    ];
+
+    address public masterChef;
+    address public incentive;
+
+    /**
+     * @dev Throws if called by any account other than the masterChef.
+     */
+    modifier onlyWhitelist() {
+        require((masterChef == msg.sender) || (incentive == msg.sender), 'ALI: caller is neither the masterChef nor incentive');
+        _;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyIncentive() {
+        require(incentive == _msgSender(), 'ALI: caller is not the incentive');
+        _;
+    }
 
     constructor(uint _initialBlockReward, uint _period) public{
         initialBlockReward = _initialBlockReward;
@@ -16,8 +47,21 @@ contract AliToken is BEP20('Alita Token', 'ALI') {
         releaseDate = now;
     }
 
-    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
-    function mint(address _to, uint256 _amount) public onlyOwner {
+    function getReleasedTokenperPeriod(uint _period) public view returns(uint){
+        return releasedTokenperPeriod[_period];
+    }
+
+    function setMasterChef(address _masterchef) public onlyOwner{
+        require(_masterchef != address(0));
+        masterChef = _masterchef;
+    }
+
+    function setIncentive(address _incentive) public onlyOwner{
+        require(_incentive != address(0));
+        incentive = _incentive;
+    }
+    
+    function mint(address _to, uint256 _amount) public onlyWhitelist {
         _mint(_to, _amount);
         _moveDelegates(address(0), _delegates[_to], _amount);
     }
